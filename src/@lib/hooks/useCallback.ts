57d48ec0d2750ae83003acccc9ec5fb5ed81f584
Paul Kang <kbu715@gmail.com>
2025-04-06 21:46:19 +0900

    fix: useCallback args type

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,12 +1,13 @@
 import type { DependencyList } from "react";
 import { useMemo } from "./useMemo";
 
-export function useCallback<T extends (...args: unknown[]) => unknown>(
-  callback: T,
-  _deps: DependencyList,
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useCallback<T extends (...args: any[]) => any>(
+	callback: T,
+	_deps: DependencyList,
 ) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  const memoizedCallback = useMemo(() => callback, _deps);
+	// 직접 작성한 useMemo를 통해서 만들어보세요.
+	const memoizedCallback = useMemo(() => callback, [..._deps]);
 
-  return memoizedCallback as T;
+	return memoizedCallback as T;
 }
