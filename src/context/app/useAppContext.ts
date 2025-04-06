697d650a83ee7a280e501e9d15c78fcca9c6a042
Paul Kang <kbu715@gmail.com>
2025-04-06 19:48:00 +0900

    refactor: 구조 변경

---
+++ src/context/app/useAppContext.ts
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { AppContext } from ".";
+
+// 커스텀 훅: useAppContext
+export const useAppContext = () => {
+	const context = useContext(AppContext);
+	if (context === undefined) {
+		throw new Error("useAppContext must be used within an AppProvider");
+	}
+	return context;
+};
